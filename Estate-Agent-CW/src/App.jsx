05c1c91a030147c0bbc703337cd14c31e9486884
Ganguli Hettiarachchi <gangulyyr@gmail.com>
2025-12-30 17:14:18 +0530

    imported json files

--- Estate-Agent-CW/src/App.jsx
+++ Estate-Agent-CW/src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css';
+import propertiesData from './data/properties.json';
+import { useState } from 'react';
 
 function App() {
+
+  const properties = propertiesData.properties;
+  const [selectedType, setSelectedType] = useState('');
+
+  // Create an empty list to store what we want to show
+  const propertyElements = [];
+
+  // Normal for loop
+  for (let i = 0; i < properties.length; i++) {
+
+    const property = properties[i];
+
+    propertyElements.push(
+      <div key={property.id} style={{ marginBottom: '20px' }}>
+        <h3>{property.type}</h3>
+        <p>Price: £{property.price}</p>
+        <p>Bedrooms: {property.bedrooms}</p>
+        <p>Location: {property.location}</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Estate Agent App</h1>
-      <p>This is my coursework projecttt.</p>
+
+      {propertyElements}
     </div>
   );
 }
